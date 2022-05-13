8c6777fb2ee055c6ffedac235300e16fc6547487
Your Name <you@example.com>
2022-05-13 23:23:47 +0200

    test API response to postman

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+// Endpoint
+app.get('/', (req, res) => res.send('API Running'));
+
+// Used for Heroku or locally on port 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
