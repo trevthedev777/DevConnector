103fb84bffcce5b62d98734a9d6a5c257bb81b32
Your Name <you@example.com>
2022-05-13 23:43:49 +0200

    connect to mongoDB with mongoose

---
+++ config/db.js
@@ -0,0 +1,24 @@
+// mongodb Connection
+const mongoose = require('mongoose');
+
+const config = require('config');
+
+// to get all values in default.json
+const db = config.get('mongoURI');
+
+// connect to MongoDB
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });
+
+        console.log('MongoDB Connected');
+    } catch(err) {
+        console.error(err.message);
+        // exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
