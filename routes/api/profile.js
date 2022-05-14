d9392ded65d88d55b9703349318020aae6d8b41f
Your Name <you@example.com>
2022-05-14 18:06:25 +0200

    Craete Get Current User Profile Route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,14 +1,31 @@
 const express = require('express');
-const res = require('express/lib/response');
-
-const Profile = require('../../routes/api/')
-
+const { defaultProxyHeaderWhiteList } = require('request/request');
 // To use the express router
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @Route       GET api/profile
-// @description TEST route
-// @access      public
-router.get('/', (req, res) => res.send('profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @Route       GET api/profile/me
+// @description Get current users profile
+// @access      Private
+router.get('/me', auth, async (req, res) => {
+
+    try {
+        // Pertains to Profile Model user id
+        const profile = await Profile.findOne({ user : req.user.id }).populate('user', ['name', 'avatar']);
+
+        if(!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user'});
+        }
+
+        // Returns the profile
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
