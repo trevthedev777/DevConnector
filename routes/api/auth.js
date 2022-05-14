65b8e031ffbb55fc1bd005976532ac4a4a9ee183
Your Name <you@example.com>
2022-05-14 03:13:01 +0200

    create auth middleware and jwt verification

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const res = require('express/lib/response');
-
 // To use the express router
 const router = express.Router();
+const auth = require('../../middleware/auth')
+
+const User = require('../../models/User')
 
 // @Route       GET api/auth
 // @description TEST route
 // @access      public
-router.get('/', (req, res) => res.send('auth route'));
+router.get('/', auth, async (req, res) => {
+
+    // return user information
+    try {
+        const user = await User
+        .findById(req.user.id)
+        .select('-password');
+        res.json(user)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error, Auth Failed')
+    }
+});
 
 module.exports = router;
