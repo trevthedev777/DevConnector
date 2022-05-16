e24c5c83af044807049e135808b4fafa68d1b6e2
Your Name <you@example.com>
2022-05-16 02:55:40 +0200

    add post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,50 @@
 const express = require('express');
-const res = require('express/lib/response');
-
 // To use the express router
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+
+// All Models
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @Route       POST api/posts
-// @description TEST route
-// @access      public
-router.get('/', (req, res) => res.send('posts route'));
+// @description Create a post
+// @access      Private
+router.post(
+  '/', [ auth, [
+    check('text', 'Text is required')
+    .not()
+    .isEmpty()
+  ]], async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      
+      const user = await User.findById(req.user.id).select('-password');
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    });
+
+    const post = await newPost.save();
+
+    res.json(post);
+    
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error: Invalid Issues');
+    }
+    
+  }
+);
 
 module.exports = router;
