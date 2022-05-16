482dd2ef41b9ea0f05f11fb064ab18eb123240be
Your Name <you@example.com>
2022-05-17 01:14:07 +0200

    Create protected route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert'
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -41,6 +43,13 @@ const App = () => {
             <Route exact path="/" element={<Landing />} />
             <Route exact path="register" element={<Register />} />
             <Route exact path="login" element={<Login />} />
+            {/* Shows Personal Info, Route is private */}
+            <Route
+            path="dashboard"
+            element={
+              <PrivateRoute component={Dashboard} />
+            }
+          />
           </Routes>
         </section>
       </Fragment>
